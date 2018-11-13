c68f457f50508f4a07673029917a472e1fd9dd14
Dylan <dybo6303@colorado.edu>
2018-11-13 16:15:24 -0700

    Adding VANTAGE class definition

---
+++ VANTAGE.m
@@ -0,0 +1,30 @@
+classdef VANTAGE < handle
+    properties
+        Deployer
+        Transform
+        OpticalCamera
+        TOFCamera
+    end
+    methods
+        function TimeStampSync()
+            
+        end
+        
+        function TOFPropagate()
+            
+        end
+        
+        function SensorFusion()
+            
+        end
+        
+        function PredictionModel()
+            
+        end
+        
+        function OutputStateVector()
+            
+        end
+        
+    end
+end
