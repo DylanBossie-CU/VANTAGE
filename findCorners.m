e0670caf30b40a4621e8212b9019b08a83819280
Dylan <dybo6303@colorado.edu>
2018-11-24 12:00:31 -0700

    cleanup

--- findCorners.m
+++
@@ -1,8 +0,0 @@
-function boundingRectangles = findCorners(boundaryPixels,I_gray)
-boundingRectangles = zeros(length(boundaryPixels),1);
-for i = 1:length(boundaryPixels)
-    boundary = boundaryPixels{i};
-    boundingRectangles(i) = ...
-        minboundrect(boundary(:,2),boundary(:,1),'p',I_gray);
-end
-end
