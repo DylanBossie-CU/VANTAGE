9e95cc2b45bebe9f89c9c48c4da794963ded37ff
Dylan <dybo6303@colorado.edu>
2018-11-27 14:23:35 -0700

    empty deployment predictions comparison function

---
+++ 4-Sensor Fusion/CDR Sensor Fusion/checkDeploymentPredictions.m
@@ -0,0 +1,8 @@
+function checkDeploymentPredictions(pos,vel,time)
+%%%% This function will validate deployment predictions by linearly
+%%%% interpolating the position and time of the first image with a visible
+%%%% CubeSat backwards to position 0, to verify that the CubeSat launched
+%%%% at the expected time.
+%%% Not going to write this right now. Algorithm in Dylan notebook p83
+
+end
