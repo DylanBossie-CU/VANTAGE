29e1c8ba0afeb6d7fb182db7f43b888347e54a95
unknown <jufa5768@colorado.edu>
2018-11-26 18:17:53 -0700

    sensor fusion

--- 4-Sensor Fusion/CDR Sensor Fusion/CDR_Sensor_Fusion_main.m
+++ 4-Sensor Fusion/CDR Sensor Fusion/CDR_Sensor_Fusion_main.m
@@ -1,5 +1,5 @@
 %% This script is being used to test the fusionCentroid function
-
+%{
 % Define outputs from camera
 camOrigin = [0 0 0];
 camVec = [1 .5 0];
@@ -13,3 +13,17 @@ sig_TOF = 0.05;
 
 % Run fusionCentroid
 [pos] = fusionCentroid(camOrigin, camVec, pos_TOF, sig_cam, sig_TOF);
+%}
+
+%% Error testing
+
+% Define truth data
+FPS = 5;
+tMax = 100;
+t = linspace(0, tMax, tMax.*FPS)';
+
+pos_init = [15 0 -5]./100;
+V_truth = [0 0 1];
+pos_truth = pos_init + V_truth.*t;
+
+
