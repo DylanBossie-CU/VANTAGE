9162ba94dda37393cbe2ff42299951b70e07dc0b
Dylan <dybo6303@colorado.edu>
2018-11-27 13:32:03 -0700

    Completed velocity calculations

--- 4-Sensor Fusion/CDR Sensor Fusion/CDR_Sensor_Fusion_main.m
+++ 4-Sensor Fusion/CDR Sensor Fusion/CDR_Sensor_Fusion_main.m
@@ -1,5 +1,7 @@
 %% This script is being used to test the fusionCentroid function
-
+clc
+close all
+clear
 % Define outputs from camera
 camOrigin = [0 0 0];
 camVec = {[1 .5 0],[1 1 0]};
@@ -15,8 +17,9 @@ sig_cam = 0.01;
 sig_TOF = 0.05;
 
 % Run fusionCentroid
-for i = 1:length(camOrigin)
-    [pos(i)] = fusionCentroid(camOrigin, camVec{i}, pos_TOF{i}, sig_cam, sig_TOF);
+pos = zeros(length(time),3);
+for i = 1:length(time)
+    [pos(i,:)] = fusionCentroid(camOrigin, camVec{i}, pos_TOF{i}, sig_cam, sig_TOF);
     if i > 1
         [vel] = calculateVelocity(pos,time);
     end
--- 4-Sensor Fusion/CDR Sensor Fusion/calculateVelocity.m
+++ 4-Sensor Fusion/CDR Sensor Fusion/calculateVelocity.m
@@ -1,4 +1,8 @@
-function velocity = calculateVelocity(positions,timestamps)
-
-velocity = 1;
+function velocityOut = calculateVelocity(positions,timestamps)
+velocity = zeros(length(positions(:,1))-1,3);
+for i = 1:length(positions(:,1))-1
+    velocity(i,:) = [positions(i+1,1)-positions(i,1),positions(i+1,2)-...
+        positions(i,2),positions(i+1,3)-positions(i,3)];
+end
+velocityOut = [mean(velocity(:,1)),mean(velocity(:,2)),mean(velocity(:,3))];
 end
