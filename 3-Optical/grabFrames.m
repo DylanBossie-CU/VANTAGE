42d8287651480fbffd769b254e8285c7ce227744
Dylan <dybo6303@colorado.edu>
2018-11-28 20:16:43 -0700

    video stuff for optic error

--- 3-Optical/grabFrames.m
+++ 3-Optical/grabFrames.m
@@ -1,8 +1,10 @@
 function grabFrames(video)
 i = 1;
 while hasFrame(video)
-    if video.currentTime == 1.0
-        frame = readFrame(video);
+    frame = readFrame(video);
+    %Grab data in intervals of 1sec
+    if video.currentTime == floor(video.currentTime)
+        %disp(video.currentTime)
         imshow(frame);
     end
     i = i + 1;
