28a5966c1ed9240e0df7f14d54c889c04dcbc321
DylanBossie <dylanbossie@gmail.com>
2018-11-29 12:26:00 -0700

    updating multi images

--- 3-Optical/FindCentroid.m
+++ 3-Optical/FindCentroid.m
@@ -14,7 +14,7 @@ for i=1:length(imageFiles)
     %}
     I_gray = rgb2gray(I);
     
-    binaryTolerance = 0.3;
+    binaryTolerance = 0.1;
     I_binarized = imbinarize(I_gray,binaryTolerance);
     imshow(I_binarized)
     [I_boundaries,~,~,~] = bwboundaries(I_binarized);
@@ -40,7 +40,7 @@ for i=1:length(imageFiles)
         %%% Plotting grayscale image overlaid with cube outline
         %% and geometric centroid overlaid
         if plotGrayscale == 1
-            %plotEdgeCentroid(objectBoundary,j)
+            plotEdgeCentroid(objectBoundary,j)
             plotBoundingCentroid(boundingRectangle,j)
         end
     end
