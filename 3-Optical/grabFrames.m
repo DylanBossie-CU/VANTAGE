c5b5e322045abe4c04aba47f638b7ddcb42de7e7
Dylan <dybo6303@colorado.edu>
2018-12-01 12:49:26 -0700

    misc

--- 3-Optical/grabFrames.m
+++ 3-Optical/grabFrames.m
@@ -3,15 +3,16 @@ function grabFrames(video)
 plotGrayscale = 1;
 plotBinarized = 0;
 i = 1;
+distanceError = [];
 while hasFrame(video)
     frame = readFrame(video);
     i = i + 1;
     %Grab data in intervals of 1sec
     if video.currentTime == floor(video.currentTime)
-        FindCentroid_Video(frame,plotGrayscale,1,plotBinarized,...
-            1,1,i)
-        imshow(frame);
+        distanceErrorFrame = FindCentroid_Video(frame,...
+            plotGrayscale,1,plotBinarized,1,1,i);
+        distanceError = [distanceError distanceErrorFrame];
+        %imshow(frame);
     end
-    
 end
 end
