e6497e727d71ac6c05b2a16ac6a312fb17d50548
Joshua Kirby <joki2454@colorado.edu>
2019-01-22 13:57:03 -0700

    added some structure

---
+++ +VANTAGE/+TOF/TOF.m
@@ -0,0 +1,9 @@
+classdef TOF 
+  %
+  % A clas representing the Time of Flight Camera used by VANTAGE
+  %
+  % 
+  
+  properties (SetAccess = public)
+    
+    % a
---
+++ +VANTAGE/+Transform/Transform.m
@@ -0,0 +1,13 @@
+classdef Transform 
+  %
+  % A class ... <DESCRIPTION>
+  %
+  %
+  
+  properties(SetAccess = private)
+    %
+    % struct containing indexed rotation matrices and translation vectors
+    % between various frames
+    
+    
+  end
