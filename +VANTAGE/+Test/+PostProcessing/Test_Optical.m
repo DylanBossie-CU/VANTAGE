7939af9c1f0224b121da28bccafc24da7718d9bf
Dylan <dybo6303@colorado.edu>
2019-02-02 13:45:26 -0700

    test updates

--- +VANTAGE/+Test/+PostProcessing/Test_Optical.m
+++ +VANTAGE/+Test/+PostProcessing/Test_Optical.m
@@ -34,11 +34,14 @@ classdef Test_Optical < matlab.unittest.TestCase
             OpticalTest = OpticalTest.setOpticalData(DesiredFPS,PlotBinarizedImages,...
                 PlotCentroids);
             
+            checkPlots = true;
+            
+            
             %%% Process input video frames through Optical class
             OpticalTest.CurrentFrameCount = 1;
             while hasFrame(OpticalTest.Video)
                 [OpticalTest,didRead] = OpticalTest.readInputFrame();
-                if didRead
+                if didRead && checkPlots == false
                     break
                 end
                 OpticalTest.CurrentFrameCount = OpticalTest.CurrentFrameCount + 1;
