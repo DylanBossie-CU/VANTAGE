1d62c76420ff9286dc3e3cbada0fea2e84a8aba1
Dylan <dybo6303@colorado.edu>
2019-02-17 13:26:33 -0700

    Updated wrapper with TOF, updated TOF code

--- VANTAGE_Wrapper.m
+++ VANTAGE_Wrapper.m
@@ -4,10 +4,13 @@
 % at the appropriate times.
 % @author   Dylan Bosie
 % @date     17-Feb-2019
+clc
+close all
+clear
 
 %% Process Deployment Manifest
 fileName = 'config/Manifest.json';
-jsondecode(fileread(fileName));
+Deployer = VANTAGE.PostProcessing.Deployer(fileName);
 
 %% Send Sensor Ready Signal to RBP
 % pleaseAvionicsTeamSaveMe(wat);
@@ -16,7 +19,20 @@ jsondecode(fileread(fileName));
 % iDontKnowHowToDoThis(yay);
 
 %% Initialize Automated Sensor Data Collection
-% SeanCodeHere(yeet);
+% SeanAutomationCodeHere(yeet);
+
+%% Data Post-Processing
+%Initialize VANTAGE Classes
+CubeSats = Deployer.CubesatArray;
+TOF = VANTAGE.PostProcessing.TOF;
+Optical = VANTAGE.PostProcessing.Optical;
+
+SensorData = jsondecode(fileread('config/Sensors.json'));
+
+% TOF Post-Processing
+TOFData = dir(strcat(SensorData.TOFData,'*.pcd'));
+TOF.naiveFindCentroids(TOFData,Deployer,SensorData);
+disp('yes')
 
 %% Compare Results against Deployment Predictions
 
