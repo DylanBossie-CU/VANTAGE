992c133f372685c887e47a47173a64731b6c6260
Dylan <dybo6303@colorado.edu>
2019-02-17 12:23:38 -0700

    sensor config file, VANTAGE wrapper

--- VANTAGE_Wrapper.m
+++ VANTAGE_Wrapper.m
@@ -6,16 +6,17 @@
 % @date     17-Feb-2019
 
 %% Process Deployment Manifest
-jsondecode();
+fileName = 'config/Manifest.json';
+jsondecode(fileread(fileName));
 
 %% Send Sensor Ready Signal to RBP
-
+% pleaseAvionicsTeamSaveMe(wat);
 
 %% Receive Launch Signal from RBP
-
+% iDontKnowHowToDoThis(yay);
 
 %% Initialize Automated Sensor Data Collection
-
+% SeanCodeHere(yeet);
 
 %% Compare Results against Deployment Predictions
 
