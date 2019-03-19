22fbdd5e90455e0a466d5b3f0a587ff99c29a433
Joshua Kirby <joki2454@colorado.edu>
2019-03-19 11:14:00 -0600

    some unit tests

--- +VANTAGE/+Test/+PostProcessing/Test_TOF.m
+++ +VANTAGE/+Test/+PostProcessing/Test_TOF.m
@@ -1,10 +1,24 @@
 classdef Test_TOF < matlab.unittest.TestCase
     %% Setup
     properties
-        validationData = load('+VANTAGE/+Test/+PostProcessing/validationData_TOF.mat');
+        validationData
+        Model
     end
     
     methods (TestClassSetup)
+        %
+        %
+        function setupValidationData(testcase)
+            load('+VANTAGE/+Test/+PostProcessing/validationData_TOF.mat');
+            if ~exist('validationData')
+                error('Loading validationData from validationData_TOF.mat failed')
+            end
+            if ~exist('Model')
+                error('Loading Model from validationData_TOF.mat failed')
+            end
+            testcase.validationData = validationData;
+            testcase.Model = Model;
+        end
     end
     
     methods (TestMethodTeardown)
@@ -12,29 +26,78 @@ classdef Test_TOF < matlab.unittest.TestCase
     %% Test methods
     methods (Test)
         
+        %
+        % Test binarySign
         function testBinarySign(testcase)
-            % Initialize Model using a simulated test case
-            configDirecName = 'Config/Testing/TOF/Simulation_TOF-Truth_3-3-19_Tube1';
-            truthFilename = 'Data/Simulation_TOF-Truth_3-3-19_Tube1/config_simulation_template_2_25_Josh_ToF_Calibration_tube1_truth_data.json';
-            manifestFilename = 'Config/Testing/TOF/Simulation_TOF-Truth_3-3-19_Tube1/Manifest_TOFdev.json';
-            Model = VANTAGE.PostProcessing.Model(manifestFilename,truthFilename,configDirecName);
-            TOF = Model.TOF;
-            % Test binarySign
+            if ~isfield(testcase.validationData,'binarySignData')
+                error('validation data does not exist for this test')
+            end
+            if isempty(testcase.validationData.binarySignData)
+                error('validation data is empty for this test')
+            end
             testVals       = testcase.validationData.binarySignData.testVals;
             validationVals = testcase.validationData.binarySignData.validationVals;
             for i = 1:length(testcase.validationData.binarySignData.testVals)
-                testcase.verifyEqual(validationVals(i),TOF.binarySign(testVals(i)));
+                testcase.verifyEqual(validationVals(i),testcase.Model.TOF.binarySign(testVals(i)));
             end
         end
         
+        %
+        % Test roundToNearestOdd
         function testRoundToNearestOdd(testcase)
-            
+            if ~isfield(testcase.validationData,'roundToNearestOddData')
+                error('validation data does not exist for this test')
+            end
+            if isempty(testcase.validationData.roundToNearestOddData)
+                error('validation data is empty for this test')
+            end
+            testVals       = testcase.validationData.roundToNearestOddData.testVals;
+            validationVals = testcase.validationData.roundToNearestOddData.validationVals;
+            for i = 1:length(testcase.validationData.roundToNearestOddData.testVals)
+                testcase.verifyEqual(validationVals(i),testcase.Model.TOF.roundToNearestOdd(testVals(i)));
+            end
+        end
+        
+        %
+        % Test minBoundingBox
+        function testMinBoundingBox(testcase)
+            if ~isfield(testcase.validationData,'minBoundingBoxData')
+                error('validation data does not exist for this test')
+            end
+            if isempty(testcase.validationData.minBoundingBoxData)
+                error('validation data is empty for this test')
+            end
+            testVals       = testcase.validationData.minBoundingBoxData.testVals;
+            validationVals = testcase.validationData.minBoundingBoxData.validationVals;
+            for i = 1:length(testcase.validationData.minBoundingBoxData.testVals)
+                testcase.verifyEqual(validationVals{i},testcase.Model.TOF.minBoundingBox(testVals{i}));
+            end
         end
             
+        %
+        % Test unitvec
+        function testUnitvec(testcase)
+            if ~isfield(testcase.validationData,'unitvecData')
+                error('validation data does not exist for this test')
+            end
+            if isempty(testcase.validationData.unitvecData)
+                error('validation data is empty for this test')
+            end
+            testVals       = testcase.validationData.unitvecData.testVals;
+            validationVals = testcase.validationData.unitvecData.validationVals;
+            for i = 1:length(testcase.validationData.unitvecData.testVals)
+                testcase.verifyEqual(validationVals{i},testcase.Model.TOF.unitvec(testVals{i}));
+            end
+        end
+        
+        %
+        % Test distInFromFace
+        
         
          
         % For development
         function testTOFProcessing(testcase)
+            return
             % housekeeping
             close all;clearvars
             rng(99);
