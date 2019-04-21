eadf262b17b021a159a8e55108b8428cd988141e
Marshall Herr <marshall.herr@colorado.edu>
2019-04-21 13:14:51 -0600

    Thanos Copter Landing

--- +VANTAGE/+PostProcessing/Validate.m
+++ +VANTAGE/+PostProcessing/Validate.m
@@ -646,62 +646,6 @@ classdef Validate
             title([TestType ' 3D Position - ' TestNum])
         end
         
-        
-    end
-    %% Private methods
-    methods (Access = private)
-        %
-        % Present TOF Errors vs Reqs
-        %
-        % @param    
-        %
-        % @return   
-        %
-        % @author   Joshua Kirby
-        % @date     19-Mar-2019
-        function TOFpresentErrorsVsReqs(~,Model,SensorData)
-            % TOF Processing
-            fileLims = [1 inf];
-            %%%%%%%%%%%%%%%%%%%%%%%%%
-            Model.Deployer = Model.TOF.TOFProcessing(SensorData,...
-                Model.Deployer,'presentResults',1,'fileLims',fileLims,'showDebugPlots',0);
-            % Get Truth Data
-            Truth = Model.Truth_VCF;
-            %%%%%%%%%%%%%%%%%%%%%%%%%
-            % Loop over cubesats
-            %%% Absolute Error Plots
-            titlefontsize = 20;
-            labelfontsize = 16;
-            figure
-            hold on
-            grid on
-            grid minor
-            xlabel('Range, m','fontsize',labelfontsize)
-            ylabel('Error, cm','fontsize',labelfontsize)
-            title(['TOF Absolute Centroid Error Vs Range'],'fontsize',titlefontsize)
-            linewidth = 2;
-            for i = 1:Truth.numCubeSats
-                % Truth Data interpolation
-                Truth.Cubesat(i).timeInterp = Model.Deployer.CubesatArray(i).time;
-                Truth.Cubesat(i).posInterp  = interp1(Truth.t,...
-                    Truth.Cubesat(i).pos,Truth.Cubesat(i).timeInterp);
-                % Calculate errors
-                Error.Cubesat(i).time = Truth.Cubesat(i).timeInterp;
-                Error.Cubesat(i).err  = vecnorm(Truth.Cubesat(i).posInterp-Model.Deployer.CubesatArray(i).centroids_VCF',2,2);
-                % Plot
-                plot(vecnorm(Truth.Cubesat(i).posInterp,2,2),Error.Cubesat(i).err.*100,'linewidth',linewidth);
-                legendstrings{i} = ['Cubesat ',num2str(i)];
-            end
-            legend(legendstrings,'location','eastoutside')
-            hold off
-            %%% Errors vs requirements plot
-            err = [];
-            for i = 1:Truth.numCubeSats
-                err = [err Error.Cubesat(i).err'];
-            end
-            onesigmastd = quantile(err,normcdf(1)-normcdf(-1));
-        end
-        
         %% Correlate the truth data of a single, most trusted CubeSat
         %
         % Preforms timestamp corrections, data rotation, and data
@@ -1957,6 +1901,61 @@ classdef Validate
             grid minor
             f.Visible = 'on';
             
+        end
+        
+    end
+    %% Private methods
+    methods (Access = private)
+        %
+        % Present TOF Errors vs Reqs
+        %
+        % @param    
+        %
+        % @return   
+        %
+        % @author   Joshua Kirby
+        % @date     19-Mar-2019
+        function TOFpresentErrorsVsReqs(~,Model,SensorData)
+            % TOF Processing
+            fileLims = [1 inf];
+            %%%%%%%%%%%%%%%%%%%%%%%%%
+            Model.Deployer = Model.TOF.TOFProcessing(SensorData,...
+                Model.Deployer,'presentResults',1,'fileLims',fileLims,'showDebugPlots',0);
+            % Get Truth Data
+            Truth = Model.Truth_VCF;
+            %%%%%%%%%%%%%%%%%%%%%%%%%
+            % Loop over cubesats
+            %%% Absolute Error Plots
+            titlefontsize = 20;
+            labelfontsize = 16;
+            figure
+            hold on
+            grid on
+            grid minor
+            xlabel('Range, m','fontsize',labelfontsize)
+            ylabel('Error, cm','fontsize',labelfontsize)
+            title(['TOF Absolute Centroid Error Vs Range'],'fontsize',titlefontsize)
+            linewidth = 2;
+            for i = 1:Truth.numCubeSats
+                % Truth Data interpolation
+                Truth.Cubesat(i).timeInterp = Model.Deployer.CubesatArray(i).time;
+                Truth.Cubesat(i).posInterp  = interp1(Truth.t,...
+                    Truth.Cubesat(i).pos,Truth.Cubesat(i).timeInterp);
+                % Calculate errors
+                Error.Cubesat(i).time = Truth.Cubesat(i).timeInterp;
+                Error.Cubesat(i).err  = vecnorm(Truth.Cubesat(i).posInterp-Model.Deployer.CubesatArray(i).centroids_VCF',2,2);
+                % Plot
+                plot(vecnorm(Truth.Cubesat(i).posInterp,2,2),Error.Cubesat(i).err.*100,'linewidth',linewidth);
+                legendstrings{i} = ['Cubesat ',num2str(i)];
+            end
+            legend(legendstrings,'location','eastoutside')
+            hold off
+            %%% Errors vs requirements plot
+            err = [];
+            for i = 1:Truth.numCubeSats
+                err = [err Error.Cubesat(i).err'];
+            end
+            onesigmastd = quantile(err,normcdf(1)-normcdf(-1));
         end
     end
 end
